1d824f2600b20151bf5ac71f7d07d2f2753d82b5
yineth damarys castillo vergara <damarys>
2025-11-21 16:59:08 -0500

    se agrago las opciones, agregar y eliminar dentro del menu.py

--- Menu.py
+++ Menu.py
@@ -11,12 +11,14 @@ class Menu:
     def mostrar_menu(self):
         while True:
             self.limpiar()
-           
-            print("1.Registrar persona: ")
-            print("2.Buscar personas por tipo de documento: ")
-            print("3.Sali: ")
+            print("--- MENÚ PRINCIPAL ---")
+            print("1. Registrar persona")
+            print("2. Buscar personas por tipo de documento")
+            print("3. Agregar persona")
+            print("4. Eliminar persona")
+            print("5. Salir")
 
-            opcion = input("Seleccione una opción: ")
+            opcion = input("Seleccione una opción: ").strip()
 
             if opcion == "1":
                 self.limpiar()
@@ -29,6 +31,16 @@ class Menu:
                 input("\nPresione ENTER para continuar...")
 
             elif opcion == "3":
+                self.limpiar()
+                self.lista.agregar_persona()
+                input("\nPresione ENTER para continuar...")
+
+            elif opcion == "4":
+                self.limpiar()
+                self.lista.eliminar_persona()
+                input("\nPresione ENTER para continuar...")
+
+            elif opcion == "5":
                 self.limpiar()
                 print("Gracias por usar el sistema. Hasta luego.")
                 break
