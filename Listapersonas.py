b44db75ed8cbf7d61a25fa694ce36959a6ff39d0
yineth damarys castillo vergara <damarys>
2025-11-21 17:12:54 -0500

    se agrego las opciones, agregar y eliminar dentro de listapersonas.py

--- Listapersonas.py
+++ Listapersonas.py
@@ -19,7 +19,7 @@ class Listapersonas:
         persona = Datos(nombre, apellido, tipo_doc, documento)
         self.lista.append(persona)
 
-        print("\n✔ Persona registrada correctamente.\n")
+        print("\n Persona registrada correctamente.\n")
 
     def buscar_por_tipo_documento(self):
         print("=== BUSCAR PERSONAS POR TIPO DE DOCUMENTO ===")
@@ -35,3 +35,27 @@ class Listapersonas:
 
         if not encontrados:
             print("No se encontraron personas con ese tipo de documento.\n")
+
+    def agregar_persona(self):
+        print("=== AGREGAR PERSONA ===")
+        nombre = input("Nombre: ")
+        apellido = input("Apellido: ")
+        tipo_doc = input("Tipo de documento: ")
+        documento = int(input("Número de documento: "))
+
+        persona = Datos(nombre, apellido, tipo_doc, documento)
+        self.lista.append(persona)
+
+        print("\n Persona agregada correctamente.\n")
+
+    def eliminar_persona(self):
+        print("=== ELIMINAR PERSONA ===")
+        documento = int(input("Ingrese el número de documento de la persona a eliminar: "))
+
+        for p in self.lista:
+            if p.documento == documento:
+                self.lista.remove(p)
+                print(f"\n Persona con documento {documento} eliminada correctamente.\n")
+                return
+
+        print("\n No se encontró ninguna persona con ese documento.\n")
